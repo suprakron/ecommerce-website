9a709195d4e28acc2cea18d29ef349069d69a0c5
atitaya <6203009@gmail.com>
2022-08-14 20:38:35 +0700

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;Planet Shopify   | Contact THAI:095-431-4798</b></span></div>
+</footer>
+    
